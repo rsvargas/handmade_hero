415c2a13ee3f2bde75bf62919a33863ed8bb5caf
Rafael Vargas <rsvargas@gmail.com>
2015-08-01 15:55:32 -0300

    day 3 - allocating the backbuffer

--- handmade/win32_handmade.cpp
+++ handmade/win32_handmade.cpp
@@ -2,6 +2,58 @@
 #include <Windows.h>
 
 
+#define internal static
+#define local_persist static;
+#define global_variable static;
+
+//TODO: global for now
+global_variable bool Running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable	void* BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void Win32ResizeDIBSection(int Width, int Height)
+{
+	//TODO: bulletproof this!
+	// maybe dont free first, free after. then free first if that fails
+
+	if (BitmapHandle)
+	{ 
+		DeleteObject(BitmapHandle);
+	}
+
+	if (!BitmapDeviceContext)
+	{
+		//TODO: should we recreate these under certains special circunstances?
+		BitmapDeviceContext = CreateCompatibleDC(0);
+	}
+
+	BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+	BitmapInfo.bmiHeader.biWidth = Width;
+	BitmapInfo.bmiHeader.biHeight = Height;
+	BitmapInfo.bmiHeader.biPlanes = 1;
+	BitmapInfo.bmiHeader.biBitCount = 32;
+	BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+	BitmapHandle = CreateDIBSection(
+		BitmapDeviceContext, &BitmapInfo,
+		DIB_RGB_COLORS,
+		&BitmapMemory,
+		0, 0);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height)
+{
+	StretchDIBits(DeviceContext,
+		X, Y, Width, Height,
+		X, Y, Width, Height,
+		BitmapMemory,
+		&BitmapInfo,
+		DIB_RGB_COLORS,
+		SRCCOPY);
+}
+
 LRESULT CALLBACK MainWindowCallback(HWND   Window,
 	UINT   Message,
 	WPARAM WParam,
@@ -11,48 +63,51 @@ LRESULT CALLBACK MainWindowCallback(HWND   Window,
 
 	switch (Message)
 	{
-	case WM_SIZE:
-		OutputDebugStringA("WM_SIZE\n");
-		break;
-
-	case WM_DESTROY:
-		OutputDebugStringA("WM_DESTROY\n");
-		break;
-
-	//case WM_CLOSE:
-	//	OutputDebugStringA("WM_CLOSE\n");
-	//	break;
-
-	case WM_ACTIVATEAPP:
-		OutputDebugStringA("WM_ACTIVATEAAPP\n");
-		break;
-
-	case WM_PAINT:
-	{
-		PAINTSTRUCT Paint;
-		HDC DeviceContext = BeginPaint(Window, &Paint);
-		int X = Paint.rcPaint.left;
-		int Y = Paint.rcPaint.top;
-		int Width= Paint.rcPaint.right - Paint.rcPaint.left;
-		int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
-		static DWORD Operation = WHITENESS;
-		PatBlt(DeviceContext, X, Y, Width, Height, Operation);
-		if (Operation == WHITENESS)
+		case WM_SIZE:
 		{
-			Operation = BLACKNESS;
-		}
-		else
+			RECT ClientRect;
+			GetClientRect(Window, &ClientRect);
+			int Width = ClientRect.right - ClientRect.left;
+			int Height = ClientRect.bottom - ClientRect.top;
+			Win32ResizeDIBSection(Width, Height);
+			OutputDebugStringA("WM_SIZE\n");
+		} break;
+
+		case WM_CLOSE:
 		{
-			Operation = WHITENESS;
-		}
-		EndPaint(Window, &Paint);
+			//TODO: handle this witha a message to the user?
+			Running = false;
+		} break;
 
-	} break;
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WM_ACTIVATEAAPP\n");
+		} break;
 
-	default:
-//		OutputDebugStringA("default\n");
-		Result = DefWindowProc(Window, Message, WParam, LParam);
-		break;
+		case WM_DESTROY:
+		{
+			//TODO: handle this with a error - recreate window?
+			Running = false;
+		} break;
+
+		case WM_PAINT:
+		{
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Width= Paint.rcPaint.right - Paint.rcPaint.left;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			Win32UpdateWindow(DeviceContext, X, Y, Width, Height);
+			EndPaint(Window, &Paint);
+
+		} break;
+
+		default:
+		{
+			//		OutputDebugStringA("default\n");
+			Result = DefWindowProc(Window, Message, WParam, LParam);
+		} break;
 	}
 
 	return Result;
@@ -92,7 +147,8 @@ int CALLBACK WinMain(HINSTANCE Instance,
 
 		if (WindowHandle)
 		{
-			for (;;)
+			Running = true;
+			while (Running)
 			{
 				MSG Message;
 				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
@@ -109,12 +165,12 @@ int CALLBACK WinMain(HINSTANCE Instance,
 		}
 		else
 		{
-			//TODO
+			//TODO: Logging
 		}
 	}
 	else
 	{
-		//TODO
+		//TODO: logging
 	}
 
 	return 0;
