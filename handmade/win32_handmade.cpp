2c82a009e075b8aa2f4b69aed70327dfa4d4423d
Rafael Vargas <rsvargas@gmail.com>
2015-08-02 02:32:59 -0300

    day 7 - initialize DirectSound

--- handmade/win32_handmade.cpp
+++ handmade/win32_handmade.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <stdint.h>
 #include <Xinput.h>
+#include <dsound.h>
 #include <stdio.h>
 
 #define internal static
@@ -61,6 +62,11 @@ global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
 #define XInputGetState XInputGetState_
 #define XInputSetState XInputSetState_
 
+
+#define DIRECT_SOUND_CREATE(name) HRESULT name(LPCGUID pcGuidDevice, LPDIRECTSOUND *ppDS, LPUNKNOWN pUnkOuter)
+typedef DIRECT_SOUND_CREATE(direct_sound_create);
+
+
 template<typename T>
 T abs(T v)
 {
@@ -81,7 +87,84 @@ internal void Win32LoadXInput()
 		XInputGetState = (x_input_get_state*)GetProcAddress(XInputLibrary,"XInputGetState");
 		XInputSetState = (x_input_set_state*)GetProcAddress(XInputLibrary, "XInputSetState");
 	}
+}
 
+internal void Win32InitDSound(HWND Window, int32 SamplesPerSecond, int32 BufferSize)
+{
+	//NOTE: Load the library
+	HMODULE DSoundLibrary = LoadLibraryA("dsound.dll");
+
+	//The primary buffer will only be used internally by direct sound
+	//we'll only be writing audio to the secondary buffer.
+
+	if (DSoundLibrary)
+	{
+		direct_sound_create *DirectSoundCreate_ = (direct_sound_create*) GetProcAddress(DSoundLibrary, "DirectSoundCreate");
+		LPDIRECTSOUND DSound;
+		if (DirectSoundCreate_ && SUCCEEDED(DirectSoundCreate_(0, &DSound, 0)))
+		{
+			WAVEFORMATEX WaveFormat = {};
+			WaveFormat.wFormatTag = WAVE_FORMAT_PCM;
+			WaveFormat.nChannels = 2;
+			WaveFormat.nSamplesPerSec = SamplesPerSecond;
+			WaveFormat.wBitsPerSample = 16;
+			WaveFormat.nBlockAlign = (WaveFormat.nChannels * WaveFormat.wBitsPerSample) / 8;
+			WaveFormat.nAvgBytesPerSec = WaveFormat.nSamplesPerSec * WaveFormat.nBlockAlign;
+			WaveFormat.cbSize = 0;
+
+			//NOTE: Get a DirectSound object! - cooperative
+			if (SUCCEEDED(DSound->SetCooperativeLevel(Window, DSSCL_PRIORITY)))
+			{
+				DSBUFFERDESC BufferDescriptor = {};
+				BufferDescriptor.dwSize = sizeof(BufferDescriptor);
+				BufferDescriptor.dwFlags = DSBCAPS_PRIMARYBUFFER;
+
+				//NOTE: Create a primary buffer
+				LPDIRECTSOUNDBUFFER PrimaryBuffer;
+				if (SUCCEEDED(DSound->CreateSoundBuffer(&BufferDescriptor, &PrimaryBuffer, 0)))
+				{
+					HRESULT Error = PrimaryBuffer->SetFormat(&WaveFormat);
+					if (SUCCEEDED(Error))
+					{
+						//NOTe: We have finally set the format
+					}
+					else
+					{
+						OutputDebugStringA("Failed set format on primary buffer\n");
+						//TODO: Diagnostic
+					}
+				}
+				else
+				{ 
+					OutputDebugStringA("Failed to create primary buffer\n");
+				}
+			}
+			else
+			{
+				OutputDebugStringA("Failed set cooperative level\n");
+				//TODO: Diagnostic
+			}
+
+			//NOTE: Create a secondary buffer
+			DSBUFFERDESC BufferDescriptor = {};
+			BufferDescriptor.dwSize = sizeof(BufferDescriptor);
+			BufferDescriptor.dwFlags = 0;
+			BufferDescriptor.dwBufferBytes = BufferSize;
+			BufferDescriptor.lpwfxFormat = &WaveFormat;
+			LPDIRECTSOUNDBUFFER SecondaryBuffer;
+			HRESULT Error = DSound->CreateSoundBuffer(&BufferDescriptor, &SecondaryBuffer, 0);
+			if (SUCCEEDED(Error))
+			{
+				//NOTE: Start it playing!
+			}
+			else
+			{
+				OutputDebugStringA("Failed to create secondary buffer\n");
+			}
+
+
+		}
+	}
 }
 
 internal win32_window_dimension Win32GetWindowDimension(HWND Window)
@@ -314,6 +397,8 @@ int CALLBACK WinMain(HINSTANCE Instance,
 			// get one device context and user it forever because we
 			// are not sharing it with anyone
 			HDC DeviceContext = GetDC(Window);
+
+			Win32InitDSound(Window, 48000, 48000* sizeof(16) * 2);
 
 			int XOffset = 0;
 			int YOffset = 0;
