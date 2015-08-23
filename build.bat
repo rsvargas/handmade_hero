0fe8a0f29a81dee60b1a191dd43baac53560e102
Rafael Vargas <rsvargas@gmail.com>
2015-08-23 15:53:12 -0300

    Enabling build with the Atom editor!

--- build.bat
+++ build.bat
@@ -1,11 +1,12 @@
 @echo off
-rem call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
 
-set CommonCompilerFlags=/MT /nologo /Gm- /GR- /EHa /Od /Oi /WX /W4 /wd4201 /wd4100 /wd4189 /wd4505 /DHANDMADE_INTERNAL=1 /DHANDMADE_SLOW=1 /Z7 
+set CommonCompilerFlags=/MT /nologo /Gm- /GR- /EHa /Od /Oi /WX /W4 /wd4201 /wd4100 /wd4189 /wd4505 /DHANDMADE_INTERNAL=1 /DHANDMADE_SLOW=1 /Z7 /FC
 set CommonLinkerFlags= -incremental:no -opt:ref  user32.lib gdi32.lib winmm.lib
 
 IF NOT EXIST build mkdir build
-pushd build
+rem pushd build
+cd build
 
 REM compilar pra winXP
 rem cl %CommonCompilerFlags% ../handmade/win32_handmade.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags%
@@ -13,7 +14,6 @@ rem cl %CommonCompilerFlags% ../handmade/win32_handmade.cpp /link -subsystem:win
 rem compilar normal x64
 del *.pdb >NUL 2> NUL
 REM Optimization switches /O2 /Oi /fp:fast
-cl /D_USRDLL /D_WINDLL %CommonCompilerFlags% ../handmade/handmade.cpp /Fmhandmade.map /LD /link /PDB:handmade_%random%.pdb /EXPORT:GameUpdateAndRender /EXPORT:GameGetSoundSamples -incremental:no
 cl %CommonCompilerFlags% ../handmade/win32_handmade.cpp /Fmwin32_handmade.map /link %CommonLinkerFlags%
-
-popd
+echo %CD%
+cl /D_USRDLL /D_WINDLL %CommonCompilerFlags% ../handmade/handmade.cpp /Fmhandmade.map /LD /link /PDB:handmade_%random%.pdb /EXPORT:GameUpdateAndRender /EXPORT:GameGetSoundSamples -incremental:no
