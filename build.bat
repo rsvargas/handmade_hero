43a9ced85452094b744dab12b183ad5a8f46a84e
Rafael Vargas <rsvargas@gmail.com>
2017-09-08 00:07:05 -0300

    Day 117 - Packing Pixels for the Framebuffer

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 setlocal
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
-set CommonCompilerFlags=/MTd /nologo /fp:fast /Gm- /GR- /EHa /Zo /Od /Oi /WX /W4 /wd4201 /wd4100 /wd4189 /wd4505 /wd4127 /DHANDMADE_INTERNAL=1 /DHANDMADE_SLOW=1 /DHANDMADE_WIN32 /Z7 /FC /F4194304
+set CommonCompilerFlags=/O2 /MTd /nologo /fp:fast /Gm- /GR- /EHa /Zo /Oi /WX /W4 /wd4201 /wd4100 /wd4189 /wd4505 /wd4127 /DHANDMADE_INTERNAL=1 /DHANDMADE_SLOW=1 /DHANDMADE_WIN32 /Z7 /FC /F4194304
 set CommonLinkerFlags= -incremental:no -opt:ref  user32.lib gdi32.lib winmm.lib
 
 if not exist code/win32_handmade.cpp goto INVALID_DIR
--- code/handmade_render_group.cpp
+++ code/handmade_render_group.cpp
@@ -538,6 +538,7 @@ internal void DrawRectangleHopefullyQuickly(loaded_bitmap *Buffer, v2 Origin, v2
     __m128 Inv255_4x = _mm_set1_ps(Inv255);
     real32 One255 = 255.0f;
     __m128 One255_4x = _mm_set1_ps(One255);
+    __m128 Half_4x = _mm_set1_ps(0.5f);
 
     __m128 One = _mm_set1_ps(1.0f);
     __m128 Zero = _mm_set1_ps(0.0f);
@@ -702,10 +703,14 @@ internal void DrawRectangleHopefullyQuickly(loaded_bitmap *Buffer, v2 Origin, v2
             __m128 l2 = _mm_mul_ps(fY, ifX);
             __m128 l3 = _mm_mul_ps(fY, fX);
 
-            __m128 Texelr = _mm_add_ps(_mm_add_ps(_mm_add_ps(_mm_mul_ps(l0, TexelAr), _mm_mul_ps(l1, TexelBr)), _mm_mul_ps(l2, TexelCr)), _mm_mul_ps(l3, TexelDr));
-            __m128 Texelg = _mm_add_ps(_mm_add_ps(_mm_add_ps(_mm_mul_ps(l0, TexelAg), _mm_mul_ps(l1, TexelBg)), _mm_mul_ps(l2, TexelCg)), _mm_mul_ps(l3, TexelDg));
-            __m128 Texelb = _mm_add_ps(_mm_add_ps(_mm_add_ps(_mm_mul_ps(l0, TexelAb), _mm_mul_ps(l1, TexelBb)), _mm_mul_ps(l2, TexelCb)), _mm_mul_ps(l3, TexelDb));
-            __m128 Texela = _mm_add_ps(_mm_add_ps(_mm_add_ps(_mm_mul_ps(l0, TexelAa), _mm_mul_ps(l1, TexelBa)), _mm_mul_ps(l2, TexelCa)), _mm_mul_ps(l3, TexelDa));
+            __m128 Texelr = _mm_add_ps(_mm_add_ps(_mm_mul_ps(l0, TexelAr), _mm_mul_ps(l1, TexelBr)),
+                                       _mm_add_ps(_mm_mul_ps(l2, TexelCr), _mm_mul_ps(l3, TexelDr)));
+            __m128 Texelg = _mm_add_ps(_mm_add_ps(_mm_mul_ps(l0, TexelAg), _mm_mul_ps(l1, TexelBg)),
+                                       _mm_add_ps(_mm_mul_ps(l2, TexelCg), _mm_mul_ps(l3, TexelDg)));
+            __m128 Texelb = _mm_add_ps(_mm_add_ps(_mm_mul_ps(l0, TexelAb), _mm_mul_ps(l1, TexelBb)),
+                                       _mm_add_ps(_mm_mul_ps(l2, TexelCb), _mm_mul_ps(l3, TexelDb)));
+            __m128 Texela = _mm_add_ps(_mm_add_ps(_mm_mul_ps(l0, TexelAa), _mm_mul_ps(l1, TexelBa)),
+                                       _mm_add_ps(_mm_mul_ps(l2, TexelCa), _mm_mul_ps(l3, TexelDa)));
 
             Texelr = _mm_mul_ps(Texelr, Colorr_4x);
             Texelg = _mm_mul_ps(Texelg, Colorg_4x);
@@ -735,19 +740,21 @@ internal void DrawRectangleHopefullyQuickly(loaded_bitmap *Buffer, v2 Origin, v2
             Blendedb = _mm_mul_ps(One255_4x, _mm_sqrt_ps(Blendedb));
             Blendeda = _mm_mul_ps(One255_4x, Blendeda);
 
-            for (int I = 0;
-                 I < 4;
-                 ++I)
-            {
-                if (ShouldFill[I])
-                {
-                    //NOTE: Repack
-                    *(Pixel + I) = (((uint32)(M(Blendeda, I) + 0.5f) << 24) |
-                                    ((uint32)(M(Blendedr, I) + 0.5f) << 16) |
-                                    ((uint32)(M(Blendedg, I) + 0.5f) << 8) |
-                                    ((uint32)(M(Blendedb, I) + 0.5f) << 0));
-                }
-            }
+            //TODO: Should we set the rounding to nearest and save the adds
+            __m128i Intr = _mm_cvttps_epi32(_mm_add_ps(Blendedr, Half_4x));
+            __m128i Intg = _mm_cvttps_epi32(_mm_add_ps(Blendedg, Half_4x));
+            __m128i Intb = _mm_cvttps_epi32(_mm_add_ps(Blendedb, Half_4x));
+            __m128i Inta = _mm_cvttps_epi32(_mm_add_ps(Blendeda, Half_4x));
+
+            Inta = _mm_slli_epi32(Inta, 24);
+            Intr = _mm_slli_epi32(Intr, 16);
+            Intg = _mm_slli_epi32(Intg, 8);
+            //Intb = _mm_slli_epi32(Inta, 0);
+
+            __m128i Out = _mm_or_si128(_mm_or_si128(Intr, Intg), _mm_or_si128(Intb, Inta));
+
+            //TODO: Write only the pixels where ShouldFill[I] == true!
+            _mm_storeu_si128((__m128i *)Pixel, Out);
 
             Pixel += 4;
         }
