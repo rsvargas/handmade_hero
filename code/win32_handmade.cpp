16aaf9351ce7fca06e6233435dda0da3124fe912
Rafael Vargas <vargas@khomp.com>
2020-09-10 00:17:33 -0300

    Day 123 - (Inro to) Interlocked Operations

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -999,13 +999,44 @@ internal void Win32DebugSyncDisplay(win32_offscreen_buffer* Backbuffer,
 }
 #endif
 
+struct work_queue_entry
+{
+    char* StringToPrint;
+};
+
+global_variable uint32 NextEntryToDo;
+global_variable uint32 EntryCount;
+work_queue_entry Entries[256];
+
+internal void PushString(char *String)
+{
+    Assert(EntryCount < ArrayCount(Entries))
+    work_queue_entry *Entry = Entries + EntryCount++;
+    Entry->StringToPrint = String;
+}
+
+struct win32_thread_info
+{
+    int LogicalThreadIndex;
+};
+
+
 DWORD WINAPI ThreadProc(LPVOID lpParameter)
 {
-    char* StringToPrint = (char*)lpParameter;
+    win32_thread_info * ThreadInfo = (win32_thread_info*) lpParameter;
     for(;;)
     {
-        OutputDebugString(StringToPrint);
-        Sleep(1000);
+        if(NextEntryToDo < EntryCount )
+        {
+            int EntryIndex = NextEntryToDo++;
+
+            work_queue_entry *Entry = Entries + EntryIndex;
+            
+            char Buffer[256];
+            wsprintf(Buffer, "Thread %u: %s\n", ThreadInfo->LogicalThreadIndex, Entry->StringToPrint);
+            OutputDebugStringA(Buffer);
+        }
+
     }
 
     //return 0;
@@ -1018,10 +1049,28 @@ int CALLBACK WinMain(HINSTANCE Instance,
 {
     win32_state Win32State = {};
 
-    char * Param = "Thread started!";
-    DWORD ThreadID;
-    HANDLE ThreadHandle = CreateThread( 0, 0, ThreadProc, Param, 0, &ThreadID );
-    CloseHandle(ThreadHandle);
+    win32_thread_info ThreadInfo[15];
+    for(int ThreadIndex = 0;
+        ThreadIndex < ArrayCount(ThreadInfo);
+        ++ThreadIndex)
+    {
+        win32_thread_info *Info = ThreadInfo + ThreadIndex;
+        Info->LogicalThreadIndex = ThreadIndex;
+        DWORD ThreadID;
+        HANDLE ThreadHandle = CreateThread( 0, 0, ThreadProc, Info, 0, &ThreadID );
+        CloseHandle(ThreadHandle);
+    }
+
+    PushString("String 0");
+    PushString("String 1");
+    PushString("String 2");
+    PushString("String 3");
+    PushString("String 4");
+    PushString("String 5");
+    PushString("String 6");
+    PushString("String 7");
+    PushString("String 8");
+    PushString("String 9");
     
     Win32GetEXEFileName(&Win32State);
 
